95427fab99e67d684444e675d3189275f9f10eab
Janice Medina <compass2k17@outlook.com>
2018-06-14 03:49:46 -0500

    docs: Comment code

--- js/app.js
+++ js/app.js
@@ -26,8 +26,13 @@ function hideModal() {
   MODAL.hidden = true;
 }
 
+/**
+ * Updates modal's displayed stats.
+ * You should usually call this
+ * before showModal()
+ */
 function updateModal() {
-  MODAL_TIME.textContent = getFormattedTime();
+  MODAL_TIME.textContent = getFormattedTime(); // TODO: remove redundant code by modularizing updateTimer function
   updateStars(getAttempts(), MODAL_STARS);
   MODAL_ATTEMPTS.textContent = getAttempts();
 }
@@ -36,12 +41,21 @@ function showModal() {
   MODAL.hidden = false;
 }
 
+/**
+ * Returns the Font Awesome icon name of this card
+ */
 function iconOfCard(card) {
   return card.children[0].getAttribute("class");
 }
 
-function cardsMatch(one, two) {
-  return iconOfCard(one) === iconOfCard(two);
+/**
+ * Cards are considered matching
+ *  when they have the same icon.
+ */
+function cardsMatch(...cards) {
+  return cards.every(c1 =>
+    cards.every(c2 => iconOfCard(c1) === iconOfCard(c2))
+  );
 }
 
 function reveal(...cards) {
@@ -56,14 +70,29 @@ function match(...cards) {
   cards.forEach(card => card.classList.add("match"));
 }
 
+/**
+ * A card is matched when:
+ * - it is not undefined
+ * - it has the class "match"
+ */
 function isMatched(card) {
   return card != undefined && card.classList.contains("match");
 }
 
+/**
+ * Pads a number.
+ * For example, the input 5 would return 05
+ * and the input 50 would return 50.
+ */
 function padNumber(number) {
   return number.toString().length == 1 ? "0" + number : number.toString();
 }
 
+/**
+ * Returns human-readable seconds.
+ * For example, if 75 seconds have elapsed,
+ * the return will be 1:15.
+ */
 function getFormattedTime() {
   let elapsed = getElapsedSeconds();
   let minutes = Math.floor(elapsed / 60);
@@ -71,10 +100,21 @@ function getFormattedTime() {
   return `${minutes}:${padNumber(seconds)}`;
 }
 
+/**
+ * Updates the score panel timer
+ * to the formatted elapsed time.
+ */
 function updateTimer() {
   SCOREBOARD_TIMER.textContent = getFormattedTime();
 }
 
+/**
+ * Sets the elapsed seconds.
+ * Reset to -1 without updates
+ * to reset the timer after a game.
+ *
+ * @param {boolean} update Whether or not to update the displayed time
+ */
 function setElapsedSeconds(seconds, update) {
   SCOREBOARD_TIMER.setAttribute("data-seconds", seconds);
   if (update) {
@@ -83,7 +123,10 @@ function setElapsedSeconds(seconds, update) {
 }
 
 let timerTask;
-
+/**
+ * Starts the timer. Increments the timer
+ * by 1 every second.
+ */
 function startTimer() {
   setElapsedSeconds(1, true);
   timerTask = setInterval(
@@ -96,17 +139,33 @@ function getElapsedSeconds() {
   return parseInt(SCOREBOARD_TIMER.getAttribute("data-seconds")) || 0;
 }
 
+/**
+ * Stops and clears the timer.
+ */
 function stopTimer() {
   clearInterval(timerTask);
   setElapsedSeconds(0, true);
 }
 
+/**
+ * Returns true if all list elements
+ * within the deck are matched as determined
+ * by isMatched(card)
+ */
 function gameIsOver() {
   return Array.from(CARDS_CONTAINER.children)
     .filter(e => e.tagName === "LI")
-    .every(c => c.classList.contains("match"));
+    .every(c => isMatched(c));
 }
 
+/**
+ * Ends the game. This will reset the game
+ * and draw a new board if the user did not
+ * win, otherwise it will show the end game
+ * modal.
+ *
+ * @param {*} wonGame Whether or not the user won the game
+ */
 function endGame(wonGame) {
   if (!wonGame) {
     resetBoard();
@@ -121,6 +180,11 @@ function endGame(wonGame) {
   setElapsedSeconds(-1, false);
 }
 
+/**
+ * Adds a new card with the given icon
+ *
+ * @param {*} icon The Font Awesome icon to use
+ */
 function addNewCard(icon) {
   let card = document.createElement("li");
   card.classList.add("card");
@@ -129,6 +193,11 @@ function addNewCard(icon) {
   return card;
 }
 
+/**
+ * Returns how many stars
+ * you get for a given amount
+ * of attempts
+ */
 function starScoreOf(attempts) {
   if (attempts <= 16) {
     return 3;
@@ -139,12 +208,21 @@ function starScoreOf(attempts) {
   }
 }
 
+/**
+ * Hides all the stars in a given element.
+ */
 function hideStars(element) {
   Array.from(element.children).forEach(
     star => (star.style.visibility = "hidden")
   );
 }
 
+/**
+ * Updates the stars of an element
+ * based on the given number of attempts.
+ * The amount of stars shown is determined
+ * by the result of starScoreOf(attempts)
+ */
 function updateStars(attempts, element) {
   let stars = starScoreOf(attempts);
   hideStars(element);
@@ -159,6 +237,17 @@ function resetBoard() {
   );
 }
 
+/**
+ * Gets the current amount of attempts.
+ * If for some reason the text content
+ * of the attempt counter could not be
+ * parsed as an int, this will default
+ * to 0.
+ */
+function getAttempts() {
+  return parseInt(ATTEMPT_COUNTER.textContent) || 0;
+}
+
 function setAttempts(number) {
   ATTEMPT_COUNTER.textContent = number;
   if (number > 1 || number <= 0) {
@@ -168,46 +257,62 @@ function setAttempts(number) {
   }
 }
 
-function getAttempts() {
-  return parseInt(ATTEMPT_COUNTER.textContent) || 0;
-}
-
+/**
+ * Draws the game by shuffling the array of icons
+ * and adding them to the game board with click logic.
+ */
 function drawGame() {
+  /*
+   * Declared here so that all 
+   * cards share the same revealed card
+   */
   let revealedCard;
 
   for (let j = 0; j < 2; j++) {
+    /*
+     * Here we abuse the sort function  
+     * to randomly shuffle the icons
+     */
     let shuffledIcons = ICONS.sort(i => Math.random() < 0.7);
     for (let i = 0; i < shuffledIcons.length; i++) {
       let card = addNewCard(shuffledIcons[i]);
 
       card.addEventListener("click", () => {
+        // Ignore clicks on same card or already matched cards
         if (revealedCard === card || isMatched(card)) {
           return;
         }
 
+        // The elapsed seconds is -1 if timer hasn't been started yet
         if (getElapsedSeconds() === -1) {
           startTimer();
         }
 
         reveal(card);
 
+        // Revealed card != undefined when a card is currently revealed
         if (revealedCard !== undefined) {
           if (cardsMatch(revealedCard, card)) {
             match(card, revealedCard);
           } else {
+            // Holds the originally revealed card until next click
             let cardToHide = revealedCard;
+            // Waiting 1ms to prevent the two cards from hiding because of this click event
             setTimeout(
               () =>
                 CARDS_CONTAINER.addEventListener(
                   "click",
-                  (hider = function(e) {
-                    if (cardToHide !== e.target) {
-                      hide(cardToHide);
-                    }
+                  (hider = e => {
+                    // If they clicked on the icon, we want its parent element (the card)
+                    let targetCard =
+                      e.target.tagName === "I"
+                        ? e.target.parentElement
+                        : e.target;
 
-                    if (card !== e.target) {
-                      hide(card);
-                    }
+                    // Hides card only if its not the target card
+                    [cardToHide, card]
+                      .filter(c => c !== targetCard)
+                      .forEach(c => hide(c));
 
                     CARDS_CONTAINER.removeEventListener("click", hider);
                   })
@@ -223,17 +328,17 @@ function drawGame() {
         }
 
         if (gameIsOver()) {
-          setTimeout(() => endGame(true), 1);
+          endGame(true);
         }
       });
     }
   }
 }
-
-RESTART_BUTTON.addEventListener("click", () => endGame(false));
++RESTART_BUTTON.addEventListener("click", () => endGame(false));
 
 MODAL_CLOSE_BUTTON.addEventListener("click", hideModal);
 
+// Restart the game by simulating clicks on the modal close and restart button
 MODAL_PLAY_AGAIN_BUTTON.addEventListener("click", () => {
   MODAL_CLOSE_BUTTON.click();
   RESTART_BUTTON.click();
